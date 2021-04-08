ebf9215a9e2b029c481afef897a0f21a73916fd3
Bryan Matteson <bryan@91.life>
2021-04-07 21:09:11 -0500

    partition now returns rects

--- algorithms.go
+++ algorithms.go
@@ -46,42 +46,81 @@ func Partition(starts, ends []float64) (ranges []Range) {
 	return
 }
 
-func PartitionRectRows(rects Rects) []Range {
+func PartitionRectRows(rects Rects) (results Rects) {
 	starts, ends := make([]float64, len(rects)), make([]float64, len(rects))
 
 	for i, r := range rects {
 		starts[i], ends[i] = r.Y.Min, r.Y.Max
 	}
 
-	return Partition(starts, ends)
+	ranges := Partition(starts, ends)
+	for _, r := range ranges {
+		grouped := make(Rects, 0)
+		for _, rect := range rects {
+			if r.ContainsRange(rect.Y) {
+				grouped = append(grouped, rect)
+			}
+		}
+		results = append(results, grouped.Union())
+	}
+	return
 }
 
-func PartitionRectColumns(rects Rects) []Range {
+func PartitionRectColumns(rects Rects) (results Rects) {
 	starts, ends := make([]float64, len(rects)), make([]float64, len(rects))
 
 	for i, r := range rects {
 		starts[i], ends[i] = r.X.Min, r.X.Max
 	}
-
-	return Partition(starts, ends)
+	ranges := Partition(starts, ends)
+	for _, r := range ranges {
+		grouped := make(Rects, 0)
+		for _, rect := range rects {
+			if r.ContainsRange(rect.X) {
+				grouped = append(grouped, rect)
+			}
+		}
+		results = append(results, grouped.Union())
+	}
+	return
 }
 
-func PartitionLineRows(lines Lines) []Range {
+func PartitionLineRows(lines Lines) (results Rects) {
 	starts, ends := make([]float64, len(lines)), make([]float64, len(lines))
 
 	for i, line := range lines {
 		starts[i], ends[i] = line.Start.Y, line.End.Y
 	}
 
-	return Partition(starts, ends)
+	ranges := Partition(starts, ends)
+	for _, r := range ranges {
+		grouped := make(Rects, 0)
+		for _, line := range lines {
+			if r.Contains(line.Start.Y) || r.Contains(line.End.Y) {
+				grouped = append(grouped, MakeRect(line.Start.X, line.Start.Y, line.End.X, line.End.Y))
+			}
+		}
+		results = append(results, grouped.Union())
+	}
+	return
 }
 
-func PartitionLineColumns(lines Lines) []Range {
+func PartitionLineColumns(lines Lines) (results Rects) {
 	starts, ends := make([]float64, len(lines)), make([]float64, len(lines))
 
 	for i, line := range lines {
 		starts[i], ends[i] = line.Start.X, line.End.X
 	}
 
-	return Partition(starts, ends)
+	ranges := Partition(starts, ends)
+	for _, r := range ranges {
+		grouped := make(Rects, 0)
+		for _, line := range lines {
+			if r.Contains(line.Start.Y) || r.Contains(line.End.Y) {
+				grouped = append(grouped, MakeRect(line.Start.X, line.Start.Y, line.End.X, line.End.Y))
+			}
+		}
+		results = append(results, grouped.Union())
+	}
+	return
 }
